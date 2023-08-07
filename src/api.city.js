d0ac57abcc2ceb33ba632914137eae089b63706c
lvapl <clashgoglvap@gmail.com>
2023-08-08 01:55:46 +0300

    Удалил папку src

--- src/api.city.js
+++
@@ -1,7 +0,0 @@
-import { instance } from "./api.config.js";
-
-export const getCity = async (city) => {
-    const {data} = await instance.get("/api/get_residential_hexagons/", {city});
-    console.log(data);
-    return data;
-}
